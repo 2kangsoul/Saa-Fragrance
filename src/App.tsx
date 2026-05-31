afddde1da2d60f22b3d2aa4fb224500ea701ef99
2kangsoul <99066423+2kangsoul@users.noreply.github.com>
2026-05-31 20:47:04 +0700

    fix header

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 // Import Layout
-import AuthLayout from "./layout/AuthLayout"; 
+import AuthLayout from "./layout/AuthLayout";
 import MainLayout from "./Features/header/component/MainLayout"; // <-- TAMBAHAN IMPORT MAINLAYOUT
 
 // Import Halaman
 import Home from "./Features/landingpages/components/Home";
 import Products from "./page/Products";
 import LoginPage from "./page/loginpage";
-import Team from "./page/Team"; 
-import Blog from "./page/Blog"; 
+import Team from "./page/Team";
+import Blog from "./page/Blog";
 import ChatBot from "./Features/chatbot/Components/ChatBot";
 import NicheGuide from "./page/niche";
 
@@ -18,23 +18,21 @@ function App() {
   return (
     <>
       <Toaster position="top-center" />
-      
+
       <Routes>
         {/* MainLayout membungkus SEMUA rute agar Header dan Footer selalu muncul */}
         <Route element={<MainLayout />}>
-          
           {/* Rute Bebas (Bisa diakses tanpa login) */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/team" element={<Team />} /> 
+          <Route path="/team" element={<Team />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/niche" element={<NicheGuide />} />
-          
+
           {/* Rute Terlindungi (Wajib Login, dicegat oleh AuthLayout) */}
           <Route element={<AuthLayout />}>
             <Route path="/products" element={<Products />} />
           </Route>
-
         </Route>
       </Routes>
 
--- src/Features/blogmanage/component/BlogManagerModal.tsx
+++ src/Features/blogmanage/component/BlogManagerModal.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useBlogManager } from "../hooks/useBlogManager";
 import type { BlogManagerModalProps } from "../types/blogManagerTypes";
 
